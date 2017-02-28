6d0338c4edf45d0211d6d3c7062fa75d9c52926a
Leandro <leandro.merlo@gmail.com>
2017-02-27 22:24:49 -0300

    Finalização do curso.

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var alternador = document.querySelector('.js-alternador');
+alternador.onclick = function(){
+	var menu = document.querySelector('.js-menu');
+	menu.classList.toggle('menu--visivel');
+};
